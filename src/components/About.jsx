21662836f1a9771d82473e3fc68d211d19fb1f19
prathamdupare <prathamdupare@proton.me>
2023-11-06 11:49:10 +0530

    new commmit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -36,25 +36,24 @@ export const About = () => {
       <div className={layout.sectionInfo}>
         <div className="absolute z-[0] w-[30%] h-[60%] -left-[10%]  rounded-full blue__gradient"></div>
         <h2 className={`pt-[50px] ${styles.heading2}`}>About the Meeting</h2>
-        <p className={`${styles.paragraph} text-justify max-w-[670px] mt-5`}>
-          This is a satellite meeting to follow the “ International Symposium on
-          Plant Photobiology” meeting in January 2024 being hosted by IISER
-          Bhopal, INDIA. The ISPP meetings started in 2001 in USA and since then
+        <p className={`${styles.paragraph} text-justify max-w-[680px] mt-5`}>
+          The EMBO-ISPP satellite meeting in January 2024 hosted by IISER Bhopal
+          , INDIA will follow the “ International Symposium on Plant
+          Photobiology”. The ISPP meetings started in 2001 in USA and since then
           the organisation responsibility circulates voluntarily between
-          scientists from Asia, the Americas and Europe every two years. It is
-          the single platform where plant photobiology leaders as well as
-          upcoming talents across the globe gather on their own interest to
-          share their latest mostly unpublished results and discuss future
-          directions. This helps the community to exchange up-to-date techniques
-          and approaches to address the different biological problems taken by
-          the different leading groups. This meeting will bring together
-          researchers from all over the globe to present their latest works on
-          various aspects of photobiology including plant photoreceptors, light
+          scientists from Asia, the Americas and Europe every two years. In this
+          meeting plant photobiology leaders as well as upcoming talents across
+          the globe gather to share their latest mostly unpublished results and
+          discuss future directions. This helps the community to exchange ideas
+          and approaches to address different biological problems related to
+          photobiology. The satellite meeting will bring together researchers
+          from all over the globe to present their latest works on various
+          aspects of photobiology including plant photoreceptors, light
           signaling, neighbour detection, flowering, circadian rhythms and
           thermomorphogenesis. The meeting intends to provide a platform for
-          research scholars to interact with world leaders in the field of Plant
-          Photobiology, foster collaboration and exchange of research ideas
-          between national and international groups.{" "}
+          research scholars to interact with world leaders in the field of plant
+          photobiology and enhance collaboration between national and
+          international groups.{" "}
         </p>
 
         <h2 className={`pt-[50px] ${styles.heading2}`}>About IISER Bhopal</h2>
